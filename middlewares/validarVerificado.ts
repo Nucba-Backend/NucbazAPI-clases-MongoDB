77278f2bfa7d77915266f6762657b031adffcfb6
Rodrigo Troncoso <rodrigo-rx@hotmail.com>
2023-07-19 14:57:21 -0300

    middleware fixes

--- middlewares/validarVerificado.ts
+++ middlewares/validarVerificado.ts
@@ -2,9 +2,9 @@ import { NextFunction, Request, Response } from "express";
 
 export const isVerified = (req: Request, res: Response, next: NextFunction) => {
 
-    const {code} = req.body.usuarioConfirmado;
+    const {verify} = req.body.usuarioConfirmado;
 
-    if (!code) {
+    if (!verify) {
         res.status(401).json({
             msg: "El usuario no está correctamente verificado"
         })
